427da3b357a2bea9b254df1bd00fe155db246f38
yrnoah <noahruan@gmail.com>
2018-12-27 14:22:28 +0800

    migrate webpack to v4

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,55 @@
+const postcss = require('postcss');
+
 module.exports = {
   plugins: [
+    require('postcss-image-set-polyfill'),
     require('autoprefixer'),
-    require('postcss-sprites'),
+    require('postcss-flexbugs-fixes'),
+    require('postcss-sprites')({
+      // retina: true,
+      // spritePath: './dist/static/react/',
+      spritesmith: {padding: 8},
+      filterBy: function(image) {
+        // 过滤图片
+        const isBg = image.url.match('background');
+        const isMap = image.url.match('map');
+        if (isBg || isMap) {
+          return Promise.reject();
+        }
+        return Promise.resolve();
+      },
+      hooks: {
+        onUpdateRule: function(rule, token, image) {
+          let backgroundSizeX = (image.spriteWidth / image.coords.width) * 100;
+          let backgroundSizeY = (image.spriteHeight / image.coords.height) * 100;
+          let backgroundPositionX = (image.coords.x / (image.spriteWidth - image.coords.width)) * 100;
+          let backgroundPositionY = (image.coords.y / (image.spriteHeight - image.coords.height)) * 100;
+
+          backgroundSizeX = isNaN(backgroundSizeX) ? 0 : backgroundSizeX;
+          backgroundSizeY = isNaN(backgroundSizeY) ? 0 : backgroundSizeY;
+          backgroundPositionX = isNaN(backgroundPositionX) ? 0 : backgroundPositionX;
+          backgroundPositionY = isNaN(backgroundPositionY) ? 0 : backgroundPositionY;
+
+          const backgroundImage = postcss.decl({
+            prop: 'background-image',
+            value: 'url(' + image.spriteUrl + ')'
+          });
+
+          const backgroundSize = postcss.decl({
+            prop: 'background-size',
+            value: backgroundSizeX + '% ' + backgroundSizeY + '%'
+          });
+
+          const backgroundPosition = postcss.decl({
+            prop: 'background-position',
+            value: backgroundPositionX + '% ' + backgroundPositionY + '%'
+          });
+
+          rule.insertAfter(token, backgroundImage);
+          rule.insertAfter(backgroundImage, backgroundPosition);
+          rule.insertAfter(backgroundPosition, backgroundSize);
+        }
+      }
+    }),
   ],
 };
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,13 +1,15 @@
+import 'dotenv/config';
 import * as webpack from 'webpack';
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 import * as path from 'path';
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config: webpack.Configuration = {
+  mode: isProd ? 'production' : 'development',
   devtool: 'source-map',
   entry: {
-    vendor: ['react', 'react-dom', 'react-router', 'react-router-dom'],
     app: ['./src/index.tsx'],
   },
   output: {
@@ -16,6 +18,18 @@ const config: webpack.Configuration = {
     filename: '[name].[hash:7].js',
     chunkFilename: '[name].[chunkhash:7].js',
     publicPath: '/',
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'initial',
+          // reuseExistingChunk: true,
+        },
+      },
+    },
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
@@ -30,48 +44,42 @@ const config: webpack.Configuration = {
       {
         test: /\.json$/,
         loader: 'json-loader',
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
       },
       {
         test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader',
-        options: {
+        // loader: 'awesome-typescript-loader',
+        loader: 'ts-loader',
+        options: isProd ? {
+          configFile:  'tsconfig.web.json',
           // useBabel: true,
-        },
+        } : undefined,
       },
       {
-        test: /\.(ttf|woff2?|svg|png|jpe?g|gif)$/,
+        test: /\.(ttf|woff2?|svg|png|jpe?g|gif|eot)$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
         },
       },
-      // {
-      //   test: /\.(ttf|woff2?|svg|png|jpe?g|gif)$/,
-      //   loader: 'file-loader',
-      //   options: {
-      //     name: '[name].[hash:7].[ext]',
-      //   },
-      // }
     ],
   },
   plugins: [
-    new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: '[name].[hash:7].js',
-    }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-      },
-    }),
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src/index.html'),
       filename: path.join(__dirname, 'dist/index.html'),
-      chunks: ['vendor', 'app'],
-      chunksSortMode: 'dependency',
+      chunksSortMode: 'none',
       minify: isProd ? {
         removeComments: true,
         useShortDoctype: true,
@@ -85,10 +93,11 @@ const config: webpack.Configuration = {
             require('google-closure-compiler-js').compile({jsCode: [{src: source}]}).compiledCode :
             '';
         },
-      } as any : undefined,
+      } : undefined,
     }),
   ],
   devServer: {
+    host: '0.0.0.0',
     hot: true,
     historyApiFallback: true,
   },
@@ -97,60 +106,71 @@ const config: webpack.Configuration = {
   },
 };
 
+const RELEASE_ID = isProd ? process.env.RELEASE_ID : undefined;
+
+const env = {
+  'process.env': {
+    NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+    RELEASE_ID: RELEASE_ID ? JSON.stringify(RELEASE_ID) : 'undefined',
+  },
+};
+
 if (isProd) {
   const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-  const ClosureCompilerPlugin = require('webpack-closure-compiler');
-  const CopyWebpackPlugin = require('copy-webpack-plugin');
-  const ExtractTextPlugin = require('extract-text-webpack-plugin');
+  const ClosurePlugin = require('closure-webpack-plugin');
   const OptimizeCssPlugin = require('optimize-css-assets-webpack-plugin');
-
-  const module: any = config.module;
-  const extractor = new ExtractTextPlugin({
+  const CleanWebpackPlugin = require('clean-webpack-plugin');
+  const extractor = new MiniCssExtractPlugin({
     filename: 'style.css',
-    allChunks: true, 
+    // allChunks: true,
   });
-  module.rules = [
-    {
-      test: /\.css?$/,
-      use: extractor.extract({
-        fallback: 'style-loader',
-        use: [
-          {loader: 'css-loader', options: {importLoaders: 1}},
-          {loader: 'postcss-loader'},
-        ],
-      }),
-    },
-    ...module.rules,
-  ];
-  config.plugins = [
-    ...config.plugins!,
-    new webpack.HashedModuleIdsPlugin(),
-    new OptimizeCssPlugin(),
-    extractor,
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    }),
-    new ClosureCompilerPlugin({
-      compiler: {
+  config.optimization = {
+    ...config.optimization,
+    minimizer: [
+      new ClosurePlugin({mode: 'STANDARD'}, {
         charset: 'utf-8',
         create_source_map: true,
         language_in: 'ECMASCRIPT5_STRICT',
         language_out: 'ECMASCRIPT5_STRICT',
-      },
+      }),
+    ],
+  };
+  config.plugins = [
+    ...config.plugins!,
+    new CleanWebpackPlugin(['dist'], {root: __dirname, verbose:  true, dry: false}),
+    new webpack.DefinePlugin(env),
+    new webpack.HashedModuleIdsPlugin(),
+    new OptimizeCssPlugin(),
+    extractor,
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
     }),
     new BundleAnalyzerPlugin({
+      // analyzerMode: 'static',
       analyzerMode: 'disabled',
-      generateStatsFile: true,
-      statsFilename: 'stats.json',
+      // generateStatsFile: true,
+      // statsFilename: 'stats.json',
     }),
-    new CopyWebpackPlugin([
-      {
-        from: path.join(__dirname, 'static'),
-        to: path.join(__dirname, 'dist'),
-        ignore: ['.*'],
-      },
-    ]),
+    // new CopyWebpackPlugin([
+    //   {
+    //     from: path.join(__dirname, 'assets'),
+    //     to: path.join(__dirname, 'dist'),
+    //     ignore: ['.*'],
+    //   },
+    // ]),
   ];
+  // if (RELEASE_ID !== 'undefined') {
+  //   config.plugins.push(new SentryPlugin({
+  //     organization: process.env.SENTRY_ORGANIZATION,
+  //     project: process.env.SENTRY_PROJECT,
+  //     apiKey: process.env.SENTRY_API_KEY,
+  //     baseSentryURL: process.env.SENTRY_BASE_URL,
+  //     release: RELEASE_ID,
+  //   }));
+  //   console.log(`upload source-map of release ${RELEASE_ID}`);
+  // } else {
+  //   console.log('RELEASE_ID is missing, skip sentry procedure.')
+  // }
 } else {
   const entry: any = config.entry;
   const module: any = config.module;
@@ -166,8 +186,9 @@ if (isProd) {
     ...module.rules,
   ];
   config.plugins = [
-    ...config.plugins,
-    new webpack.HotModuleReplacementPlugin(),
+    ...config.plugins!!,
+    new webpack.DefinePlugin(env),
+    new webpack.NamedModulesPlugin(),
   ];
   if (entry instanceof Object) {
     Object.keys(entry).filter(key => key !== 'vendor').forEach(key => {
